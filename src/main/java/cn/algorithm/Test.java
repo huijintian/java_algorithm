16fb5b413d185e3a6c7fafd1e480e889e3ab1a85
mengtian <wu750316@163.com>
2018-05-30 17:32:06 +0800

    add test

---
+++ src/main/java/cn/algorithm/Test.java
@@ -0,0 +1,10 @@
+package cn.algorithm;
+
+/**
+ * Created by mengtian on 2018/5/30
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("test add");
+    }
+}
